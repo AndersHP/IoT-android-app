48a91b7403b1b8f1de9289ef7cf8bcde7e2fe5e0
Alexander Peev <peev.alexander@gmail.com>
2017-04-26 16:21:24 +0200

    Added AM2302.h

---
+++ AM2302.h
@@ -0,0 +1,20 @@
+#include "application.h"
+	
+class AM2302 {
+ public:
+  AM2302(int pin);
+  bool begin(void);
+  float readTemperature(void);
+  float readHumidity(void);
+  bool readTemperatureAndHumidity(float&, float&);
+
+ private:
+  bool readData(void);
+  bool parityCorrect(uint8_t reply[5]);
+  int waitForHigh(int limitMicroSec);
+  int waitForLow(int limitMicroSec);
+  uint8_t readBit(void);
+  uint8_t readByte(void);
+  float humidity, temp;
+  int pin;
+};
